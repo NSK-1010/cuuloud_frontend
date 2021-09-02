f116ae757905a6da89e8952e5e74ca2ce50569b6
TMP-tenpura <tmptenpura@gmail.com>
2021-09-02 21:29:11 +0900

    UIの修正

--- src/components/ChatObject.vue
+++ src/components/ChatObject.vue
@@ -3,45 +3,46 @@
   <div :id="'msarea-'+roomId"
   class="scrollbar" style="
   overflow: auto;
-  height: calc(99vh - 172px);">
+  height: calc(99vh - 206px);">
     <v-card
     style="margin:10px;"
     outlined
-    :class="{
-      'center': chat.type !== 'message',
-      'left': chat.user_id === userId && chat.type === 'message',
-      'right': chat.user_id !== userId && chat.type === 'message',
-    }"
     v-for="chat in chatlog" :key="chat.created_at">
       <v-card-text>
-        <v-list-item class="grow">
+        <v-card-text class="text-h6" v-if="chat.type === 'message'">
+          {{ chat.text }}
+        </v-card-text>
+        <v-card-text class="text-h6" v-if="chat.type === 'join'">
+          {{ chat.user_name }} が参加しました
+        </v-card-text>
+        <v-card-text class="text-h6" v-if="chat.type === 'leave'">
+          {{ chat.user_name }} が退出しました
+        </v-card-text>
+        <v-list-item class="grow" v-if="chat.type === 'message'">
+          <v-list-item-avatar color="grey darken-3">
+            <v-img
+              class="elevation-6"
+              alt=""
+              src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+            ></v-img>
+          </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title>
-              {{ showDate(chat.created_at) }}
-            </v-list-item-title>
+            <v-list-item-title class="text-h7">{{ chat.user_name }}</v-list-item-title>
           </v-list-item-content>
-          <v-row
-          align="center"
-          justify="end"
-          >
-            <span class="subheading" v-if="chat.type === 'message'">
-              {{ chat.user_name }}
-            </span>
+          <v-row align="center" justify="end">
+            <span class="subheading mr-2 text-h7">{{ showDate(chat.created_at) }}</span>
           </v-row>
         </v-list-item>
       </v-card-text>
-      <v-card-text v-if="chat.type === 'message'">
-        {{ chat.text }}
-      </v-card-text>
-      <v-card-text class="text-h5" v-if="chat.type === 'join'">
-        {{ chat.user_name }} が参加しました
-      </v-card-text>
-      <v-card-text class="text-h5" v-if="chat.type === 'leave'">
-        {{ chat.user_name }} が退出しました
-      </v-card-text>
     </v-card>
   </div>
-  <v-card tile height="64">
+  <v-card tile outlined height="98">
+      <v-card tile height="32">
+        <v-card-actions>
+          <v-icon @click="leave">mdi-logout-variant</v-icon>
+          <v-icon>mdi-image</v-icon>
+        </v-card-actions>
+    </v-card>
     <v-card-actions>
       <v-text-field height="48" outlined v-model="text" append-outer-icon="mdi-send"
       @keyup.enter="send" @click:append-outer="send" />
@@ -50,28 +51,11 @@
 </div>
 </template>
 <style scoped>
-.left {
-  clear:  both;
-  float: left;
-  width: 40vw;
-}
-
-.right {
-  float: right;
-  clear:  both;
-  width: 40vw;
-}
-
-.center {
-  margin-left: auto;
-  margin-right: auto;
-  width: 70vw;
-}
 </style>
 <script>
 export default {
   name: 'ChatObject',
-  props: ['name', 'roomId', 'userId'],
+  props: ['name', 'roomId'],
   mounted() {
     this.$on('recieve', (data) => {
       const messagesArea = document.getElementById(`msarea-${this.roomId}`);
@@ -86,6 +70,9 @@ export default {
     });
   },
   methods: {
+    leave() {
+      this.$emit('leave', this.roomId);
+    },
     send() {
       this.$emit('send', this.roomId, this.text);
       this.text = '';
--- src/components/HomeObject.vue
+++ src/components/HomeObject.vue
@@ -2,10 +2,21 @@
   <div class="scrollbar" style="
   overflow: auto;
   height: calc(100vh - 112px);">
+    <v-card style="margin:10px;" elevation="7" shaped>
+      <v-card-title>部屋を作成する</v-card-title>
+      <v-card-text>
+        <v-form @submit.prevent>
+          <v-text-field label="部屋の名前" v-model="roomName" @keyup.enter="createRoom"/>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn class="info" @click="createRoom">作成</v-btn>
+      </v-card-actions>
+    </v-card>
     <v-card style="margin:10px;" elevation="7" shaped v-for="room in rooms" :key="room.created_at">
       <v-card-title>{{ room.name }}</v-card-title>
       <v-card-actions>
-        <v-btn @click="join(room.id)">参加</v-btn>
+        <v-icon @click="join(room.id)">mdi-logout-variant</v-icon>
       </v-card-actions>
     </v-card>
   </div>
@@ -25,9 +36,14 @@ export default {
     join(room) {
       this.$emit('join', room);
     },
+    createRoom() {
+      this.$emit('create', this.roomName);
+      this.roomName = '';
+    },
   },
   data() {
     return {
+      roomName: '',
       rooms: [],
     };
   },
