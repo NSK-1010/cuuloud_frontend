e355b9a5f783af3bc890fb7bf53cdd10f379d186
TMP-tenpura <tmptenpura@gmail.com>
2021-09-05 06:27:29 +0900

    色の変更

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#00b4ff', // #E53935
+        secondary: '#00a9ff', // #FFCDD2
+        accent: '#006dce', // #3F51B5
+      },
+      dark: {
+        primary: '#00b4ff', // #E53935
+        secondary: '#00a9ff', // #FFCDD2
+        accent: '#006dce', // #3F51B5
+      },
+    },
+  },
 });
