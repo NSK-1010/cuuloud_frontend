2b36cac6a54176284ac70957125b9c863913cbb4
TMP-tenpura <tmptenpura@gmail.com>
2021-09-06 01:33:02 +0900

    アイコンにホバーした際、説明を表示するよう変更

--- src/components/TopBarWidgets.vue
+++ src/components/TopBarWidgets.vue
@@ -1,42 +1,47 @@
 <template>
   <div>
-    <v-btn text v-if="!isLogin" @click="$emit('login')">
-      <v-badge :value="login" color="deep-purple accent-4"
-      content="ログイン" transition="slide-x-transition" >
-        <v-hover v-model="login">
-          <v-icon>mdi-login</v-icon>
-        </v-hover>
-      </v-badge>
-    </v-btn>
-    <v-btn text v-if="!isLogin" @click="$emit('register')">
-      <v-badge :value="register" color="deep-purple accent-4"
-      content="登録" transition="slide-x-transition" >
-        <v-hover v-model="register">
+    <v-hover v-model="login">
+      <v-btn icon v-if="!isLogin" @click="$emit('login')">
+        <v-badge class="hover-badge" :value="login" color="accent"
+        content="ログイン" transition="slide-x-transition" >
+            <v-icon>mdi-login</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-hover>
+    <v-hover v-model="register">
+      <v-btn icon v-if="!isLogin" @click="$emit('register')">
+        <v-badge class="hover-badge" :value="register" color="accent"
+        content="登録" transition="slide-x-transition" >
           <v-icon>mdi-account-plus</v-icon>
-        </v-hover>
-      </v-badge>
-    </v-btn>
-    <v-btn icon v-if="isLogin" @click="$emit('invite')">
-      <v-badge :value="invite" color="deep-purple accent-4"
-      content="招待" transition="slide-x-transition" >
-        <v-hover v-model="invite">
+        </v-badge>
+      </v-btn>
+    </v-hover>
+    <v-hover v-model="invite">
+      <v-btn icon v-if="isLogin" @click="$emit('invite')">
+        <v-badge class="hover-badge" :value="invite" color="accent"
+        content="招待" transition="slide-x-transition" >
           <v-icon>mdi-account-multiple-plus</v-icon>
-        </v-hover>
-      </v-badge>
-    </v-btn>
-    <v-btn text v-if="isLogin" @click="$emit('logout')">
-      <v-badge :value="logout" color="deep-purple accent-4"
-      content="ログアウト" transition="slide-x-transition" >
-        <v-hover v-model="logout">
+        </v-badge>
+      </v-btn>
+    </v-hover>
+    <v-hover v-model="logout">
+      <v-btn icon v-if="isLogin" @click="$emit('logout')">
+        <v-badge class="hover-badge" :value="logout" color="accent"
+        content="ログアウト" transition="slide-x-transition" >
           <v-icon>mdi-logout</v-icon>
-        </v-hover>
-      </v-badge>
-    </v-btn>
+        </v-badge>
+      </v-btn>
+    </v-hover>
     <v-avatar v-if="isLogin" color="secondary" size="48">
       <span class="white--text text-h5">{{ name.substr(0,2) }}</span>
     </v-avatar>
   </div>
 </template>
+<style scoped>
+.hover-badge {
+  z-index:1;
+}
+</style>
 <script>
 export default {
   name: 'TopBarWidgets',
