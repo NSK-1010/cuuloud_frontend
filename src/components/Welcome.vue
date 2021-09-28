4b28561240cd2743e91b657d28f144e2bef92a8c
TMP-tenpura <tmptenpura@gmail.com>
2021-09-29 02:02:42 +0900

    ウェルカムページの修正

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="py-auto my-16" style="height:calc(100vh - 64px);">
+  <div class="py-auto my-16">
     <div>
       <p class="font-weight-light text-center text-h2">
         Cuuloudへようこそ。
+      </p>
+      <p class="my-8 font-weight-regular text-center">
+        ここは、今だけの話題でゆったりと話すことができる場所です。<br>
+        左上のボタンから、早速始めてみましょう！
       </p>
       <v-img
             alt="Culoud Logo"
@@ -10,12 +14,8 @@
             contain
             :src="require('@/assets/logo_full.png')"
             transition="scale-transition"
-            width="50vw"
+            width="40vw"
             />
-      <p class="my-8 font-weight-regular text-center">
-        ここは、今だけの話題でゆったりと話すことができる場所です。<br>
-        左上のボタンから、早速始めてみましょう！
-      </p>
     </div>
   </div>
 </template>
