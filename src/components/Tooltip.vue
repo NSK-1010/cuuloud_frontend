005728179767aacc6691c4065a07c4043903c896
TMP-tenpura <tmptenpura@gmail.com>
2021-10-08 19:53:12 +0900

    ツールチップをコンポーネント化

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+      <div v-bind="attrs" v-on="on">
+        <slot />
+      </div>
+    </template>
+    <span>{{ text }}</span>
+  </v-tooltip>
+</template>
+<script>
+export default {
+  name: 'Tooltip',
+  props: ['text'],
+};
+</script>
